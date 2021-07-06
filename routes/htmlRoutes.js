ce1939cbeedb786e188c4c61f0294d76f6f091c4
Drew Selden <seldend1@gmail.com>
2021-07-06 02:05:19 -0400

    html routes updated

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+
+// GET requests for html
+router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,23 +12,15 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "Develop/public")));
+app.use(express.static(path.join(__dirname, 'Develop/public')));
+app.use('/', htmlRoutes);
+app.use('/api/notes', apiRoutes);
 
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the Star Wars Page!');
-//   });
-  
-// app.get('/about', (req, res) => {
-//     res.send("This is the about page");
-//   })
 
-// GET requests for html
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
-
-app.get('/api/notes', (req, res) => res.sendFile(path.json(__dirname, "Develop/db/db.json")));
+app.get('/api/notes', (req, res) => res.sendFile(path.json(__dirname, "../db/db.json")));
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
